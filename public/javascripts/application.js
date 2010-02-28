e8950168cd2a2077fb6e6c8a3d3764301437eac9
Alban Peignier <alban@tryphon.eu>
2010-02-28 16:41:25 +0100

    Improve Network#method selection in edit
     * use radio buttons to select method
     * use a fieldset to regroup static method inputs
     * disable static inputs when dhcp method is selected

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,20 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+Event.observe(window, "load", function() {
+  var toggle_network_static_inputs = function(enable) {
+    $('network_static_inputs').select("input[type=text]").each(function(input) {
+      if (enable) {
+        input.enable();
+      } else {
+        input.disable();
+      }
+    });
+  };
+
+  $$('input[type=radio][name="network[method]"]').each(function(radio) {
+    if (radio.value == "static") {
+      toggle_network_static_inputs(radio.checked);
+    }
+    Event.observe(radio, 'change', function() {
+      toggle_network_static_inputs(radio.getValue() == "static");
+    });
+  });
+});
--- spec/views/networks/edit.html.erb_spec.rb
+++ spec/views/networks/edit.html.erb_spec.rb
@@ -16,4 +16,26 @@ describe "/networks/edit" do
     response.should have_tag('a[href=?]', network_path)
   end
 
+  it "should display a radio button to select static method" do
+    render 'networks/edit'
+    response.should have_tag('input[type=radio][value=static][name=?]','network[method]')
+  end
+
+  it "should display a label for static method radio button with NetworkPresentation static method name" do
+    NetworkPresenter.stub!(:static_method_name).and_return("static_method_name")
+    render 'networks/edit'
+    response.should have_tag('label','static_method_name')
+  end
+
+  it "should display a radio button to select dhcp method" do
+    render 'networks/edit'
+    response.should have_tag('input[type=radio][value=dhcp][name=?]','network[method]')
+  end
+
+  it "should display a label for dhcp method radio button with NetworkPresentation dhcp method name" do
+    NetworkPresenter.stub!(:dhcp_method_name).and_return("dhcp_method_name")
+    render 'networks/edit'
+    response.should have_tag('label','dhcp_method_name')
+  end
+
 end
