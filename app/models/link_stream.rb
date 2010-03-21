61ca154444898fe2b59ba5c606cdad88d5c27bab
Alban Peignier <alban@tryphon.eu>
2010-03-21 13:51:08 +0100

    Use HostValidation for LinkStream#target_host

--- app/models/link_stream.rb
+++ app/models/link_stream.rb
@@ -14,6 +14,8 @@ class LinkStream < ActiveForm::Base
   cattr_accessor :default_http_port
 
   attr_accessor :target_host
+  include HostValidation
+  validates_host :target_host
 
   include ActsAsIpPort
   acts_as_ip_port :target_port, :udp_port, :http_port, :user_port => true
@@ -24,7 +26,6 @@ class LinkStream < ActiveForm::Base
   end
 
   before_validation :use_default_ports
-  validate :must_found_target_host
 
   def use_default_ports
     self.target_port ||= default_udp_port
@@ -34,20 +35,6 @@ class LinkStream < ActiveForm::Base
 
   def new_record?
     false
-  end
-
-  def must_found_target_host
-    return unless errors.on(target_host).blank?
-
-    begin
-      if target_host =~ /^[0-9\.]+$/
-        IPAddr.new(target_host, Socket::AF_INET)
-      else
-        Socket.gethostbyname(target_host)
-      end
-    rescue
-      errors.add(:target_host, :not_valid_hostname)
-    end
   end
 
 end
